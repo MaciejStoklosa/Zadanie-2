5153b90e6764127c55682b8f2a69f3f4430bd2dc
unknown <you@example.com>
2018-09-19 01:03:31 +0200

    Zadanie 2 zrobione

---
+++ Zadanie 2/Zadanie 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+      
+         public static long SilniaRekurencyjna (int liczba){
+            if(liczba > 1){
+                return liczba * SilniaRekurencyjna(liczba - 1);
+            }else{
+                return 1;
+            }
+        }
+        public static long SilniaIteracyjnie (int liczba){
+            long silnia = 1;
+
+            for ( int i = 1 ; i <=liczba ; i ++  ){
+                silnia*=i;
+            }
+            return silnia;
+        }
+        static void Main(string[] args)
+        {
+              long x = SilniaIteracyjnie(8);  
+              long  s = SilniaRekurencyjna(8);
+        }
+    }
+}
